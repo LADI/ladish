d694bb15d9f89018148d10d26b3e22330df672d1
dave <dave@a436a847-0d15-0410-975c-d299462d15a1>
2006-10-04 14:45:31 +0000

    Added toplevel autogen.sh.
    git-svn-id: http://svn.drobilla.net/lad@155 a436a847-0d15-0410-975c-d299462d15a1

---
+++ autogen.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+top=`pwd`
+
+echo 'Generating necessary files (recursively)...'
+
+aclocal &
+
+echo -n "* flowcanvas * \t"
+cd flowcanvas
+./autogen.sh
+cd $top
+
+echo -n "* libslv2 * \t"
+cd libslv2
+./autogen.sh
+cd $top
+
+echo -n "* omins * \t"
+cd omins
+./autogen.sh
+cd $top
+
+echo -n "* ingen * \t"
+cd ingen
+./autogen.sh
+cd $top
+
+automake --foreign --add-missing
+autoconf
+
