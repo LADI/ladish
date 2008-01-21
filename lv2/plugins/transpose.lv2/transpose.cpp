32f9877be33dd2ec219a54fd7e43b7e5f56d20fd
drobilla <drobilla@a436a847-0d15-0410-975c-d299462d15a1>
2008-01-21 15:14:53 +0000

    Work on generic LV2 events.
    git-svn-id: http://svn.drobilla.net/lad@1090 a436a847-0d15-0410-975c-d299462d15a1

---
+++ lv2/plugins/transpose.lv2/transpose.cpp
@@ -0,0 +1,38 @@
+/* An LV2 plugin which transposes MIDI notes.
+ * Copyright (C) 2008 Dave Robillard
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <iostream>
+#include "lv2/lv2plugin.hpp"
+#include "lv2/event/lv2_event.h"
+
+using namespace std;
+
+class Transpose : public LV2::Plugin<Transpose> {
+public:
+	Transpose(double rate)
+		: LV2::Plugin<Transpose>(3)
+	{}
+
+	void run(uint32_t sample_count) {
+		LV2_Event_Buffer* in = p<LV2_Event_Buffer>(0);
+		if (in->event_count > 0)
+			cout << "Transpose received " << in->event_count << " events."  << endl;
+	}
+};
+
+static unsigned _ = Transpose::register_class(
+		"http://drobilla.net/lv2_plugins/dev/transpose");
