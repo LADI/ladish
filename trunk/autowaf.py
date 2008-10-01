d876a8383cb24a2a49d8ea078450042c4fcbe47c
drobilla <drobilla@a436a847-0d15-0410-975c-d299462d15a1>
2008-10-01 04:43:04 +0000

    Documentation building.
    git-svn-id: http://svn.drobilla.net/lad@1572 a436a847-0d15-0410-975c-d299462d15a1

--- trunk/autowaf.py
+++ trunk/autowaf.py
@@ -2,6 +2,7 @@
 # Waf utilities for easily building standard unixey packages/libraries
 # Coyright (C) 2008 Dave Robillard (see COPYING file for details)
 
+import os
 import misc
 
 def configure(conf):
@@ -13,6 +14,7 @@ def link_flags(env, lib):
 def compile_flags(env, lib):
 	return ' '.join(map(lambda x: env['CPPPATH_ST'] % x, env['CPPPATH_' + lib]))
 
+# Pkg-config file
 def build_pc(bld, name, version, libs):
 	'''Build a pkg-config file for a library.
 	name    -- uppercase variable name     (e.g. 'SOMENAME')
@@ -36,4 +38,23 @@ def build_pc(bld, name, version, libs):
 	for i in libs:
 		obj.dict[i + '_LIBS']   = link_flags(bld.env(), i)
 		obj.dict[i + '_CFLAGS'] = compile_flags(bld.env(), i)
+
+# Doxygen API documentation
+def build_dox(bld, name, version, srcdir, blddir):
+	obj = bld.create_obj('subst')
+	obj.source = 'doc/reference.doxygen.in'
+	obj.target = 'doc/reference.doxygen'
+	obj.dict = {
+		name + '_VERSION' : version,
+		name + '_SRCDIR'  : os.path.abspath(srcdir),
+		name + '_DOC_DIR' : os.path.abspath(blddir + '/default/doc')
+	}
+	obj.inst_var = 0
+	out1 = bld.create_obj('command-output')
+	out1.stdout = '/doc/doxygen.out'
+	out1.stdin = '/doc/reference.doxygen' # whatever..
+	out1.command = 'doxygen'
+	out1.argv = [os.path.abspath(blddir) + '/default/doc/reference.doxygen']
+	out1.command_is_external = True
+
 
