81d5e8de18acb89739b47c8fff4e7e49b0422b5b
Nedko Arnaudov <nedko@arnaudov.name>
2009-07-30 01:16:07 +0300

    unify source copyright headers in gladish

--- gui/PatchageCanvas.cpp
+++ gui/PatchageCanvas.cpp
@@ -1,18 +1,27 @@
-/* This file is part of Patchage.
+/* -*- Mode: C ; c-basic-offset: 2 -*- */
+/*
+ * LADI Session Handler (ladish)
+ *
  * Copyright (C) 2007 Dave Robillard <http://drobilla.net>
- * 
- * Patchage is free software; you can redistribute it and/or modify it under the
- * terms of the GNU General Public License as published by the Free Software
- * Foundation; either version 2 of the License, or (at your option) any later
- * version.
- * 
- * Patchage is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for details.
- * 
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin St, Fifth Floor, Boston, MA  02110-1301, USA
+ *
+ **************************************************************************
+ * This file contains implementation of the canvas class
+ **************************************************************************
+ *
+ * LADI Session Handler is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * LADI Session Handler is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LADI Session Handler. If not, see <http://www.gnu.org/licenses/>
+ * or write to the Free Software Foundation, Inc.,
+ * 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
 #include "config.h"
--- gui/lash_proxy.hpp
+++ gui/lash_proxy.hpp
@@ -1,19 +1,27 @@
-// -*- Mode: C++ ; indent-tabs-mode: t -*-
-/* This file is part of Patchage.
+/* -*- Mode: C ; c-basic-offset: 2 -*- */
+/*
+ * LADI Session Handler (ladish)
+ *
  * Copyright (C) 2008 Nedko Arnaudov <nedko@arnaudov.name>
- * 
- * Patchage is free software; you can redistribute it and/or modify it under the
- * terms of the GNU General Public License as published by the Free Software
- * Foundation; either version 2 of the License, or (at your option) any later
- * version.
- * 
- * Patchage is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for details.
- * 
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin St, Fifth Floor, Boston, MA  02110-1301, USA
+ *
+ **************************************************************************
+ * This file contains interface to code that interfaces ladishd through D-Bus
+ **************************************************************************
+ *
+ * LADI Session Handler is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * LADI Session Handler is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LADI Session Handler. If not, see <http://www.gnu.org/licenses/>
+ * or write to the Free Software Foundation, Inc.,
+ * 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
 #ifndef LASH_PROXY_HPP__89E81B38_627F_41B9_AD08_DB119FB5F34C__INCLUDED
