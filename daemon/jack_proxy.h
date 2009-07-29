de37a1ef33a46d0449176d522babd21807ff528e
Nedko Arnaudov <nedko@arnaudov.name>
2009-07-29 20:45:43 +0300

    Add copyright headers to the reverntly added new files

--- daemon/jack_proxy.h
+++ daemon/jack_proxy.h
@@ -1,10 +1,29 @@
 /* -*- Mode: C ; c-basic-offset: 2 -*- */
-/*****************************************************************************
+/*
+ * LADI Session Handler (ladish)
  *
- * DESCRIPTION:
- *  Helper functionality for accessing JACK through D-Bus
+ * Copyright (C) 2009 Nedko Arnaudov <nedko@arnaudov.name>
  *
- *****************************************************************************/
+ **************************************************************************
+ * This file contains interface to the helper functionality for accessing
+ * JACK through D-Bus
+ **************************************************************************
+ *
+ * LADI Session Handler is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * LADI Session Handler is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LADI Session Handler. If not, see <http://www.gnu.org/licenses/>
+ * or write to the Free Software Foundation, Inc.,
+ * 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
 
 #ifndef JACK_PROXY_H__88702EEC_4B82_407F_A664_AD70C1E14D02__INCLUDED
 #define JACK_PROXY_H__88702EEC_4B82_407F_A664_AD70C1E14D02__INCLUDED
