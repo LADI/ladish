5e94e9dccc759bd5d9e5c3d81bf73a60a12f3b8d
Nedko Arnaudov <nedko@arnaudov.name>
2009-07-29 21:08:27 +0300

    unify source copyright headers in ladishd

--- daemon/sigsegv.c
+++ daemon/sigsegv.c
@@ -1,14 +1,27 @@
-/**
- * This source file is used to print out a stack-trace when your program
- * segfaults.  It is relatively reliable and spot-on accurate.
+/* -*- Mode: C ; c-basic-offset: 2 -*- */
+/*
+ * LADI Session Handler (ladish)
  *
- * This code is in the public domain.  Use it as you see fit, some credit
- * would be appreciated, but is not a prerequisite for usage.  Feedback
- * on it's use would encourage further development and maintenance.
+ * Copyright (C) 2005 - 2008 Jaco Kroon <jaco@kroon.co.za>
  *
- * Author:  Jaco Kroon <jaco@kroon.co.za>
+ **************************************************************************
+ * This file contains code to print out a stack-trace when program segfaults.
+ **************************************************************************
  *
- * Copyright (C) 2005 - 2008 Jaco Kroon
+ * LADI Session Handler is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * LADI Session Handler is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LADI Session Handler. If not, see <http://www.gnu.org/licenses/>
+ * or write to the Free Software Foundation, Inc.,
+ * 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
 #include "config.h"
