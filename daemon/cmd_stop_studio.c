c1869c29a66ef56f20feef5ff8bc1c41b02e7dea
Nedko Arnaudov <nedko@arnaudov.name>
2009-12-06 14:35:59 +0200

    daemon: better handling of jack server stop failure
    sometimes jack server stop request fails, even if it does actually stop
    shortly after. in this case ladish will wait 5 seconds before giving up.

--- daemon/cmd_stop_studio.c
+++ daemon/cmd_stop_studio.c
@@ -29,25 +29,29 @@
 #include "cmd.h"
 #include "studio_internal.h"
 
-#define cmd_ptr ((struct ladish_command *)context)
+struct ladish_command_stop_studio
+{
+  struct ladish_command command;
+  int stop_waits;
+};
+
+#define cmd_ptr ((struct ladish_command_stop_studio *)context)
 
 static bool run(void * context)
 {
   bool jack_server_started;
 
-  switch (cmd_ptr->state)
+  switch (cmd_ptr->command.state)
   {
   case LADISH_COMMAND_STATE_PENDING:
     if (!studio_is_started())
     {
       /* nothing to do, studio is not running */
-      cmd_ptr->state = LADISH_COMMAND_STATE_DONE;
+      cmd_ptr->command.state = LADISH_COMMAND_STATE_DONE;
       return true;
     }
 
     ladish_app_supervisor_stop(g_studio.app_supervisor);
-
-    usleep(3000000);
 
     log_info("Stopping JACK server...");
 
@@ -55,13 +59,43 @@ static bool run(void * context)
 
     if (!jack_proxy_stop_server())
     {
-      log_error("Stopping JACK server failed.");
-      return false;
+      log_error("Stopping JACK server failed. Waiting stop for 5 seconds anyway...");
+
+      /* JACK server stop sometimes fail, even if it actually succeeds after some time */
+      /* Reproducable with yoshimi-0.0.45 */
+
+      cmd_ptr->stop_waits = 5000;
+      cmd_ptr->command.state = LADISH_COMMAND_STATE_WAITING;
+      return true;
     }
 
-    cmd_ptr->state = LADISH_COMMAND_STATE_WAITING;
+    cmd_ptr->command.state = LADISH_COMMAND_STATE_WAITING;
     /* fall through */
   case LADISH_COMMAND_STATE_WAITING:
+    if (cmd_ptr->stop_waits > 0)
+    {
+      if (!jack_proxy_is_started(&jack_server_started))
+      {
+        log_error("jack_proxy_is_started() failed.");
+        return false;
+      }
+
+      if (!jack_server_started)
+      {
+        goto done;
+      }
+
+      if (cmd_ptr->stop_waits == 1)
+      {
+        log_error("JACK server stop wait after stop request expired.");
+        return false;
+      }
+
+      cmd_ptr->stop_waits--;
+      usleep(1000);
+      return true;
+    }
+
     if (!ladish_environment_consume_change(&g_studio.env_store, ladish_environment_jack_server_started, &jack_server_started))
     {
       /* we are still waiting for the JACK server stop */
@@ -69,6 +103,7 @@ static bool run(void * context)
       return true;
     }
 
+  done:
     log_info("Wait for JACK server stop complete.");
 
     ASSERT(!jack_server_started);
@@ -77,7 +112,7 @@ static bool run(void * context)
 
     on_event_jack_stopped();
 
-    cmd_ptr->state = LADISH_COMMAND_STATE_DONE;
+    cmd_ptr->command.state = LADISH_COMMAND_STATE_DONE;
     return true;
   }
 
@@ -89,18 +124,19 @@ static bool run(void * context)
 
 bool ladish_command_stop_studio(void * call_ptr, struct ladish_cqueue * queue_ptr)
 {
-  struct ladish_command * cmd_ptr;
+  struct ladish_command_stop_studio * cmd_ptr;
 
-  cmd_ptr = ladish_command_new(sizeof(struct ladish_command));
+  cmd_ptr = ladish_command_new(sizeof(struct ladish_command_stop_studio));
   if (cmd_ptr == NULL)
   {
     lash_dbus_error(call_ptr, LASH_DBUS_ERROR_GENERIC, "ladish_command_new() failed.");
     goto fail;
   }
 
-  cmd_ptr->run = run;
+  cmd_ptr->command.run = run;
+  cmd_ptr->stop_waits = -1;
 
-  if (!ladish_cqueue_add_command(queue_ptr, cmd_ptr))
+  if (!ladish_cqueue_add_command(queue_ptr, &cmd_ptr->command))
   {
     lash_dbus_error(call_ptr, LASH_DBUS_ERROR_GENERIC, "ladish_cqueue_add_command() failed.");
     goto fail_destroy_command;
