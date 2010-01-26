fb5f17037f3afd46491814c394b39ba107d34d5e
Nedko Arnaudov <nedko@arnaudov.name>
2010-01-26 21:57:59 +0200

    definitions of well known ports

---
+++ lib/wkports.h
@@ -0,0 +1,54 @@
+/* -*- Mode: C ; c-basic-offset: 2 -*- */
+/*
+ * LADI Session Handler (ladish)
+ *
+ * Copyright (C) 2010 Nedko Arnaudov <nedko@arnaudov.name>
+ *
+ **************************************************************************
+ * This file contains definitions of well known ports
+ **************************************************************************
+ *
+ * LADI Session Handler is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * LADI Session Handler is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LADI Session Handler. If not, see <http://www.gnu.org/licenses/>
+ * or write to the Free Software Foundation, Inc.,
+ * 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef WKPORTS_H__0C316912_FC11_4B34_A8DB_5F9C359ACA03__INCLUDED
+#define WKPORTS_H__0C316912_FC11_4B34_A8DB_5F9C359ACA03__INCLUDED
+
+/* 2776ef6f-39e1-4c5e-8920-3deeb409c3f5 */
+UUID_DEFINE(ladish_wkport_playback_left,0x27,0x76,0xEF,0x6F,0x39,0xE1,0x4C,0x5E,0x89,0x20,0x3D,0xEE,0xB4,0x09,0xC3,0xF5);
+
+/* b371a9a1-18cb-4932-813c-daf278cf2b20 */
+UUID_DEFINE(ladish_wkport_playback_right,0xB3,0x71,0xA9,0xA1,0x18,0xCB,0x49,0x32,0x81,0x3C,0xDA,0xF2,0x78,0xCF,0x2B,0x20);
+
+/* f50862dd-3071-4727-be1c-3c9a115d2141 */
+UUID_DEFINE(ladish_wkport_monitor_left,0xF5,0x08,0x62,0xDD,0x30,0x71,0x47,0x27,0xBE,0x1C,0x3C,0x9A,0x11,0x5D,0x21,0x41);
+
+/* 35aac0d6-189d-412d-91a1-0a17212cbdb4 */
+UUID_DEFINE(ladish_wkport_monitor_right,0x35,0xAA,0xC0,0xD6,0x18,0x9D,0x41,0x2D,0x91,0xA1,0x0A,0x17,0x21,0x2C,0xBD,0xB4);
+
+/* 871954a7-7c0f-4092-ae34-465e1f1bb0ca */
+UUID_DEFINE(ladish_wkport_capture_left,0x87,0x19,0x54,0xA7,0x7C,0x0F,0x40,0x92,0xAE,0x34,0x46,0x5E,0x1F,0x1B,0xB0,0xCA);
+
+/* 2dc1da00-cebd-47ea-a239-c30db9653e7c */
+UUID_DEFINE(ladish_wkport_capture_right,0x2D,0xC1,0xDA,0x00,0xCE,0xBD,0x47,0xEA,0xA2,0x39,0xC3,0x0D,0xB9,0x65,0x3E,0x7C);
+
+/* 04f5fa90-f453-479d-8f3a-2f92cf78c604 */
+UUID_DEFINE(ladish_wkport_midi_playback,0x04,0xF5,0xFA,0x90,0xF4,0x53,0x47,0x9D,0x8F,0x3A,0x2F,0x92,0xCF,0x78,0xC6,0x04);
+
+/* abc90af1-dcc4-41b3-b3eb-d4f5a9c4f708 */
+UUID_DEFINE(ladish_wkport_midi_capture,0xAB,0xC9,0x0A,0xF1,0xDC,0xC4,0x41,0xB3,0xB3,0xEB,0xD4,0xF5,0xA9,0xC4,0xF7,0x08);
+
+#endif /* #ifndef WKPORTS_H__0C316912_FC11_4B34_A8DB_5F9C359ACA03__INCLUDED */
