LADI/ladish

commit ebce8139970bf6646858b10c4e7d4f9d00fbdb20

Fix unused-parameter warnings in plain C code

daemon/graph.c
@@ -1303,16 +1303,16 @@ bool ladish_graph_client_looks_empty_internal(struct ladish_graph * graph_ptr, s
     port_ptr = list_entry(node_ptr, struct ladish_graph_port, siblings_client);
     if (!port_ptr->hidden)
     {
-      //log_info("port '%s' is visible, client '%s' does not look empty", port_ptr->name, client_ptr->name);
+      log_debug("port '%s' is visible, client '%s' does not look empty", port_ptr->name, client_ptr->name);
       return false;
     }
     else
     {
-      //log_info("port '%s' is invisible", port_ptr->name);
+      log_debug("port '%s' is invisible", port_ptr->name);
     }
   }
 
-  //log_info("client '%s' looks empty in graph %s", client_ptr->name, graph_ptr->opath != NULL ? graph_ptr->opath : "JACK");
+  log_debug("client '%s' looks empty in graph %s", client_ptr->name, graph_ptr->opath != NULL ? graph_ptr->opath : "JACK");
   return true;
 }
 
@@ -2975,9 +2975,9 @@ void ladish_graph_trick_dicts(ladish_graph_handle graph_handle)
 static
 bool
 ladish_graph_copy_client_begin_callback(
-  void * context,
+  void * UNUSED(context),
   ladish_graph_handle graph_handle,
-  bool hidden,
+  bool UNUSED(hidden),
   ladish_client_handle client_handle,
   const char * client_name,
   void ** client_iteration_context_ptr_ptr)
@@ -2989,7 +2989,7 @@ ladish_graph_copy_client_begin_callback(
     return false;
   }
 
-  if (!ladish_graph_add_client(context, copy, client_name, false))
+  if (!ladish_graph_add_client(graph_handle, copy, client_name, false))
   {
     ladish_client_destroy(copy);
     return false;
@@ -3003,12 +3003,12 @@ ladish_graph_copy_client_begin_callback(
 static
 bool
 ladish_graph_copy_port_callback(
-  void * context,
+  void * UNUSED(context),
   ladish_graph_handle graph_handle,
-  bool hidden,
+  bool UNUSED(hidden),
   void * client_iteration_context_ptr,
-  ladish_client_handle client_handle,
-  const char * client_name,
+  ladish_client_handle UNUSED(client_handle),
+  const char * UNUSED(client_name),
   ladish_port_handle port_handle,
   const char * port_name,
   uint32_t port_type,
@@ -3021,7 +3021,7 @@ ladish_graph_copy_port_callback(
     return false;
   }
 
-  if (!ladish_graph_add_port(context, client_iteration_context_ptr, copy, port_name, port_type, port_flags, true))
+  if (!ladish_graph_add_port(graph_handle, client_iteration_context_ptr, copy, port_name, port_type, port_flags, true))
   {
     ladish_port_destroy(copy);
     return false;

cdbus/helpers.c
@@ -2,7 +2,7 @@
 /*
  * LADI Session Handler (ladish)
  *
- * Copyright (C) 2008,2009,2010,2011 Nedko Arnaudov <nedko@arnaudov.name>
+ * Copyright (C) 2008,2009,2010,2011,2012 Nedko Arnaudov <nedko@arnaudov.name>
  * Copyright (C) 2008 Juuso Alasuutari <juuso.alasuutari@gmail.com>
  *
  **************************************************************************
@@ -34,8 +34,7 @@
 
 #include "helpers.h"
 #include "method.h"
-#include "../log.h"
-#include "../assert.h"
+#include "../common.h"
 #include "../common/klist.h"
 
 /* D-Bus versions earlier than 1.4.12 dont define DBUS_TIMEOUT_INFINITE */
@@ -765,7 +764,7 @@ find_signal_hook_descriptor(
 static
 DBusHandlerResult
 cdbus_signal_handler(
-  DBusConnection * connection_ptr,
+  DBusConnection * UNUSED(connection_ptr),
   DBusMessage * message_ptr,
   void * data)
 {

cdbus/object_path.c
@@ -161,7 +161,11 @@ void cdbus_introspection_destroy(struct cdbus_object_path *path)
   }
 }
 
-static bool cdbus_introspection_handler(const struct cdbus_interface_descriptor * interface, struct cdbus_method_call * call_ptr)
+static
+bool
+cdbus_introspection_handler(
+  const struct cdbus_interface_descriptor * UNUSED(interface),
+  struct cdbus_method_call * call_ptr)
 {
   if (strcmp(call_ptr->method_name, "Introspect") != 0)
   {
@@ -401,7 +405,7 @@ handled:
   return DBUS_HANDLER_RESULT_HANDLED;
 }
 
-static void cdbus_object_path_handler_unregister(DBusConnection * connection_ptr, void * data)
+static void cdbus_object_path_handler_unregister(DBusConnection * UNUSED(connection_ptr), void * data)
 {
   log_debug("Message handler of object path %s was unregistered", (opath_ptr && opath_ptr->name) ? opath_ptr->name : "<unknown>");
 }

common.h
@@ -2,7 +2,7 @@
 /*
  * LADI Session Handler (ladish)
  *
- * Copyright (C) 2009,2011 Nedko Arnaudov <nedko@arnaudov.name>
+ * Copyright (C) 2009,2011,2012 Nedko Arnaudov <nedko@arnaudov.name>
  *
  **************************************************************************
  * This file contains stuff that is needed almost everywhere in ladish
@@ -57,5 +57,6 @@ extern const char * g_app_L2_js_constant;
 const char * ladish_map_app_level_constant(const char * level);
 
 #define LADISH_PUBLIC __attribute__ ((visibility ("default")))
+#define UNUSED(x) UNUSED_ ## x __attribute__((unused))
 
 #endif /* #ifndef COMMON_H__82C9504A_ACD2_435D_9743_781943473E6A__INCLUDED */

common/log.c
@@ -168,9 +168,9 @@ static
 bool
 ladish_log_enabled(
   unsigned int level,
-  const char * file,
-  unsigned int line,
-  const char * func)
+  const char * UNUSED(file),
+  unsigned int UNUSED(line),
+  const char * UNUSED(func))
 {
   return level != LADISH_LOG_LEVEL_DEBUG;
 }

conf.c
@@ -2,7 +2,7 @@
 /*
  * LADI Session Handler (ladish)
  *
- * Copyright (C) 2010,2011 Nedko Arnaudov <nedko@arnaudov.name>
+ * Copyright (C) 2010,2011,2012 Nedko Arnaudov <nedko@arnaudov.name>
  *
  **************************************************************************
  * This file contains implementation of the settings storage
@@ -147,7 +147,7 @@ bool install_term_signal_handler(int signum, bool ignore_if_already_ignored)
   return true;
 }
 
-int main(int argc, char ** argv)
+int main(int UNUSED(argc), char ** UNUSED(argv))
 {
   if (getenv("HOME") == NULL)
   {

daemon/check_integrity.c
@@ -2,7 +2,7 @@
 /*
  * LADI Session Handler (ladish)
  *
- * Copyright (C) 2010 Nedko Arnaudov <nedko@arnaudov.name>
+ * Copyright (C) 2010,2012 Nedko Arnaudov <nedko@arnaudov.name>
  *
  **************************************************************************
  * This file contains the code that checks data integrity
@@ -45,12 +45,12 @@ static void ladish_check_integrity_fail(const char * message)
 
 bool
 ladish_check_vgraph_integrity_client_begin_callback(
-  void * context,
-  ladish_graph_handle graph_handle,
-  bool hidden,
-  ladish_client_handle client_handle,
-  const char * client_name,
-  void ** client_iteration_context_ptr_ptr)
+  void * UNUSED(context),
+  ladish_graph_handle UNUSED(graph_handle),
+  bool UNUSED(hidden),
+  ladish_client_handle UNUSED(client_handle),
+  const char * UNUSED(client_name),
+  void ** UNUSED(client_iteration_context_ptr_ptr))
 {
   return true;
 }
@@ -59,9 +59,9 @@ bool
 ladish_check_vgraph_integrity_port_callback(
   void * context,
   ladish_graph_handle vgraph,
-  bool hidden,
-  void * client_iteration_context_ptr,
-  ladish_client_handle client_handle,
+  bool UNUSED(hidden),
+  void * UNUSED(client_iteration_context_ptr),
+  ladish_client_handle UNUSED(client_handle),
   const char * client_name,
   ladish_port_handle vport,
   const char * port_name,
@@ -100,19 +100,23 @@ ladish_check_vgraph_integrity_port_callback(
 
 bool
 ladish_check_vgraph_integrity_client_end_callback(
-    void * context,
-    ladish_graph_handle graph_handle,
-    bool hidden,
-    ladish_client_handle client_handle,
-    const char * client_name,
-    void * client_iteration_context_ptr)
+  void * UNUSED(context),
+  ladish_graph_handle UNUSED(graph_handle),
+  bool UNUSED(hidden),
+  ladish_client_handle UNUSED(client_handle),
+  const char * UNUSED(client_name),
+  void * UNUSED(client_iteration_context_ptr))
 {
   return true;
 }
 
 #undef ctx_ptr
 
-bool ladish_check_vgraph_integrity(void * context, ladish_graph_handle graph, ladish_app_supervisor_handle app_supervisor)
+bool
+ladish_check_vgraph_integrity(
+  void * context,
+  ladish_graph_handle graph,
+  ladish_app_supervisor_handle UNUSED(app_supervisor))
 {
   ladish_graph_iterate_nodes(
     graph,

daemon/cmd_change_app_state.c
@@ -70,7 +70,12 @@ static bool run_target_start(struct ladish_command_change_app_state * cmd_ptr, l
   return true;
 }
 
-static bool run_target_stop(struct ladish_command_change_app_state * cmd_ptr, ladish_app_supervisor_handle supervisor, ladish_app_handle app)
+static
+bool
+run_target_stop(
+  struct ladish_command_change_app_state * cmd_ptr,
+  ladish_app_supervisor_handle UNUSED(supervisor),
+  ladish_app_handle app)
 {
   const char * app_name;
   uuid_t app_uuid;

commit 0120d3162be294c5d969e43144964f63ce5f2b8f

Fix room creation that got broken two commits ago
ebce8139970bf6646858b10c4e7d4f9d00fbdb20 broke it.

daemon/graph.c
@@ -2975,8 +2975,8 @@ void ladish_graph_trick_dicts(ladish_graph_handle graph_handle)
 static
 bool
 ladish_graph_copy_client_begin_callback(
-  void * UNUSED(context),
-  ladish_graph_handle graph_handle,
+  void * context,
+  ladish_graph_handle UNUSED(graph_handle),
   bool UNUSED(hidden),
   ladish_client_handle client_handle,
   const char * client_name,
@@ -2989,7 +2989,7 @@ ladish_graph_copy_client_begin_callback(
     return false;
   }
 
-  if (!ladish_graph_add_client(graph_handle, copy, client_name, false))
+  if (!ladish_graph_add_client(context, copy, client_name, false))
   {
     ladish_client_destroy(copy);
     return false;
@@ -3003,8 +3003,8 @@ ladish_graph_copy_client_begin_callback(
 static
 bool
 ladish_graph_copy_port_callback(
-  void * UNUSED(context),
-  ladish_graph_handle graph_handle,
+  void * context,
+  ladish_graph_handle UNUSED(graph_handle),
   bool UNUSED(hidden),
   void * client_iteration_context_ptr,
   ladish_client_handle UNUSED(client_handle),
@@ -3021,7 +3021,7 @@ ladish_graph_copy_port_callback(
     return false;
   }
 
-  if (!ladish_graph_add_port(graph_handle, client_iteration_context_ptr, copy, port_name, port_type, port_flags, true))
+  if (!ladish_graph_add_port(context, client_iteration_context_ptr, copy, port_name, port_type, port_flags, true))
   {
     ladish_port_destroy(copy);
     return false;