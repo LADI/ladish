e5efbfc472c68d624680658c670f4f3166bf0cec
Nedko Arnaudov <nedko@arnaudov.name>
2012-12-02 23:52:43 +0200

    Add missing header text

--- daemon/load.c
+++ daemon/load.c
@@ -1,13 +1,28 @@
 /* -*- Mode: C ; c-basic-offset: 2 -*- */
-/*****************************************************************************
+/*
+ * LADI Session Handler (ladish)
  *
- * DESCRIPTION:
- *  
+ * Copyright (C) 2009,2010,2011,2012 Nedko Arnaudov <nedko@arnaudov.name>
  *
- * NOTES:
- *  
+ **************************************************************************
+ * This file contains implementation for the load helper functions
+ **************************************************************************
  *
- *****************************************************************************/
+ * LADI Session Handler is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * LADI Session Handler is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LADI Session Handler. If not, see <http://www.gnu.org/licenses/>
+ * or write to the Free Software Foundation, Inc.,
+ * 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
 
 #include "load.h"
 #include "limits.h"
