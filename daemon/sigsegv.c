d562213ddde8d99aa21bd05ab102e39d60db048d
Nedko Arnaudov <nedko@arnaudov.name>
2012-12-22 21:53:38 +0200

    (Hopeuflly) fix comilation warning/error
    ../daemon/sigsegv.c: В функции ‘signal_segv’:
    ../daemon/sigsegv.c:114:9: ошибка: формат ‘%02d’ предполагает тип ‘int’, но аргумент 6 имеет тип ‘size_t’

--- daemon/sigsegv.c
+++ daemon/sigsegv.c
@@ -111,7 +111,9 @@ static void signal_segv(int signum, siginfo_t* info, void*ptr) {
     log_error("No stack trace");
 #else
     for(i = 0; i < NGREG; i++)
-        log_error("reg[%02d]       = 0x" REGFORMAT, i, ucontext->uc_mcontext.gregs[i]);
+    {
+      log_error("reg[%02d]       = 0x" REGFORMAT, i, (int)ucontext->uc_mcontext.gregs[i]);
+    }
 
 #if defined(SIGSEGV_STACK_X86) || defined(SIGSEGV_STACK_IA64)
 # if defined(SIGSEGV_STACK_IA64)
