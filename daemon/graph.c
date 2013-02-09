fcb16ae71e8b4211b657d864f613d59e1df03321
Nedko Arnaudov <nedko@arnaudov.name>
2013-02-09 21:06:56 +0200

    minor coding style fix

--- daemon/graph.c
+++ daemon/graph.c
@@ -2,7 +2,7 @@
 /*
  * LADI Session Handler (ladish)
  *
- * Copyright (C) 2008,2009,2010,2011,2012 Nedko Arnaudov <nedko@arnaudov.name>
+ * Copyright (C) 2008,2009,2010,2011,2012,2013 Nedko Arnaudov <nedko@arnaudov.name>
  * Copyright (C) 2008 Juuso Alasuutari
  *
  **************************************************************************
@@ -1835,7 +1835,7 @@ ladish_client_handle ladish_graph_find_client_by_app(ladish_graph_handle graph_h
   list_for_each(node_ptr, &graph_ptr->clients)
   {
     client_ptr = list_entry(node_ptr, struct ladish_graph_client, siblings);
-    if(! ladish_client_get_app(client_ptr->client, current_uuid))
+    if (!ladish_client_get_app(client_ptr->client, current_uuid))
       continue;
 
     if (uuid_compare(current_uuid, app_uuid) == 0)
