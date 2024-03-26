9fb169b73657b28e6a118fb10968ffaabe271d1a
Yuri Victorovich <yuri@FreeBSD.org>
2024-03-26 20:16:31 +0200

    Appply patch-proxies_graph__proxy.h from FreeBSD

--- proxies/graph_proxy.h
+++ proxies/graph_proxy.h
@@ -28,6 +28,7 @@
 #define GRAPH_PROXY_H__61D1ED56_E33B_4F50_B45B_F520979E8AA7__INCLUDED
 
 #include "common.h"
+#include <unistd.h>
 
 typedef struct graph_proxy_tag { int unused; } * graph_proxy_handle;
 
